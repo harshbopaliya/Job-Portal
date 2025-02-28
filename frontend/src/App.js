5c914f46b15b6b49effd84c156a3a0036669e5e4
harsh Bopaliya <bopaliyaharsh7@gmail.com>
2025-02-28 13:20:21 +0530

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import JobListings from "./pages/JobListings";
-// import Login from "./pages/login";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/jobs" element={<JobListings />} />
       </Routes>
     </Router>
